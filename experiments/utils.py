da6d116f255ad3dfbe78b299a368fce10a91039d
Christoph Schoeller <schoeller@fortiss.org>
2019-03-21 12:34:05 +0100

    Refactored copy_code function in utils.

--- experiments/utils.py
+++ experiments/utils.py
@@ -3,38 +3,39 @@ import os.path as path
 from shutil import copyfile
 
 
-def copy_code(sources_path, backup_path, exclude_dirs=None):
+def copy_code(source_dir, dest_dir, exclude_dirs=['__pycache__', '.git', 'experiments'], exclude_files=['.pyc']):
     """
-    Creates a copy of the code currently used to run in the provided directory.
-    Folders specified in exclude_dirs are not copied.
+    Copies code from source_dir to dest_dir. Excludes specified folders and files by substring-matching.
     """
-    # Never copy cache.
-    if not exclude_dirs:
-        exclude_dirs = []
-    exclude_dirs.append('__pycache__')
+    source_basename = path.basename(source_dir)
+    for root, dirs, files in os.walk(source_dir, topdown=True):
 
-    for root, dirs, files in os.walk(sources_path, topdown=True):
+        # skip ignored dirs
+        if any(ex_subdir in root for ex_subdir in exclude_dirs):
+            continue
 
-        # Remove excluded folders in_place within the walk iterator.        
-        dirs[:] = [d for d in dirs if d not in exclude_dirs]
+        # construct destination dir
+        cropped_root = root[2:] if (root[:2] == './') else root
+        subdir_basename = path.basename(cropped_root)
 
-        # Strip leading local directory and construct destination directory.
-        cropped_root = root[2:] if (root[:2] == './') else root 
-        dest_dir = os.path.join(backup_path, cropped_root)     
+        # do not treat the root as a subdir
+        if subdir_basename == source_basename:
+            subdir_basename = ""
+        dest_subdir = os.path.join(dest_dir, subdir_basename)
 
-        # Create target folder.
-        if not os.path.exists(dest_dir):
-            os.makedirs(dest_dir)
+        # create destination folder
+        if not os.path.exists(dest_subdir):
+            os.makedirs(dest_subdir)
 
-        # Copy files.
-        for filename in filter(lambda x: not '.pyc' in x, files): #lambda x: '.py' in x and not '.pyc' in x, files
+        # copy files
+        for filename in filter(lambda x: not any(substr in x for substr in exclude_files), files):
             source_file_path = os.path.join(root, filename)
-            dest_file_path = os.path.join(dest_dir, filename)
+            dest_file_path = os.path.join(dest_subdir, filename)
             copyfile(source_file_path, dest_file_path)
 
 def retrieve_git_hash():
     """
-    Gets and returns the current gith hash.
+    Retrieves and returns the current gith hash.
     """
     try:
         git_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
