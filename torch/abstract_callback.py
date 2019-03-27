ab0c021a3ff31c3571619a9ce0e563c6ccedc14d
Christoph Schoeller <schoeller@fortiss.org>
2019-03-27 17:27:14 +0100

    Fixed comments in AbstractCallback.

--- torch/abstract_callback.py
+++ torch/abstract_callback.py
@@ -1,8 +1,24 @@
-class AbstractCallback:
 
-    def __call__(self, epoch, step, loss, context):
+
+class AbstractCallback:
+    """
+    Interface that defines how callbacks must be specified.
+    """
+
+    def __call__(self, epoch, step, performance_measures, context):
+        """
+        Called after every batch by the ModelTrainer.
+
+        Parameters:
+            epoch (int): current epoch number
+            step (int): current batch number
+            performance_measures (dict): losses and metrics based on a running average
+            context (ModelTrainer): reference to the caller, allows manipulations
+        """
         raise NotImplementedError
 
     def close(self):
-        """ Handle cleanup work if necessary. Will be called after training. """
+        """
+        Handle cleanup work if necessary. Will be called at the end of the last epoch.
+        """
         pass
